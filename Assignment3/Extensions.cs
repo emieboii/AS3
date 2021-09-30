aab2e8a882ebbd972d0cd0f532cecb30ae7aef32
Emil Gullaksen <emie@live.dk>
2021-09-30 12:23:48 +0200

    test

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -4,5 +4,9 @@ namespace BDSA2020.Assignment02
 {
     public static class Extensions
     {
+        //IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items);
+        public IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) = ()
+
+        //IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate);
     }
 }
